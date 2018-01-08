a36566e7299d6956b354058a301ed84ba52c8fda
ouqunfu <ouqunfu@foxmail.com>
2018-01-08 16:41:58 +0800

    加载css

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import './style.css';
 
 function component() {
   var element = document.createElement('div');
@@ -6,6 +7,7 @@ function component() {
   // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
 
   return element;
 }
